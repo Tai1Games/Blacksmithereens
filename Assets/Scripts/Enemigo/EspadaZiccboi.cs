b8415cc5e5469239116e9aef36bb3ebb4d1dfbd5
Pj <pjurad01@ucm.es>
2019-03-27 19:17:43 +0100

    Creado ziccboi funcional
    Todo debería funcionar correctamente.

---
+++ Assets/Scripts/Enemigo/EspadaZiccboi.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EspadaZiccboi : MonoBehaviour {
+    
+    private GameObject jugador;
+    public int daño;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        VidaJugador vida;
+        vida = col.GetComponent<VidaJugador>();   //coge referencia al objeto colisionado
+
+        if (vida != null)
+        {
+            vida.RestaVida(daño); //Al colisionar con el jugador, le resta vida
+        }
+    }
+}
--- Assets/Scripts/Enemigo/Ziccboi.cs
+++ Assets/Scripts/Enemigo/Ziccboi.cs
@@ -8,15 +8,22 @@ using UnityEngine;
 public class Ziccboi : MonoBehaviour {
 
     public float velocidad;
+    public float offsetAtaque;
+    public float tiempoEspera;
+
     private Rigidbody2D rb;
     private Vector2 movimiento;
     private GameObject jugador;
     private Vector2 diferencia;
     private float angulo;
+    Animator anim;
+    bool atacando = false;
+
 	void Start ()
     {
         rb = GetComponent<Rigidbody2D>();
         jugador = LevelManager.instance.Jugador(); //recibe una referencia del jugador
+        anim = GetComponentInChildren<Animator>();
     }
 	
 	void Update ()
@@ -26,6 +33,13 @@ public class Ziccboi : MonoBehaviour {
         diferencia = new Vector2(jugador.transform.position.x - transform.position.x, jugador.transform.position.y - transform.position.y);
         angulo = Mathf.Atan2(diferencia.x, diferencia.y) * Mathf.Rad2Deg; //angulo a traves de la tangente y lo pasa a grados
         transform.rotation = Quaternion.Euler(0, 0, -angulo); //cambia la rotacion del enemigo
+        if(rb.position.x > jugador.transform.position.x - offsetAtaque && rb.position.y > jugador.transform.position.y - offsetAtaque &&
+                    rb.position.x < jugador.transform.position.x + offsetAtaque && rb.position.y < jugador.transform.position.y + offsetAtaque
+                    && atacando == false) //Si el ziccboi está a menos de una determinada distancia del jugador y no está atacando ya...
+        {
+            atacando = true;
+            StartCoroutine(ZiccboiAtaca()); //Comienza la corrutina de ataque
+        }
     }
 
     private void FixedUpdate()
@@ -37,5 +51,16 @@ public class Ziccboi : MonoBehaviour {
             //mueve al enemigo asegurandose de que no supera la velocidad si se mueve en diagonal
             rb.velocity = Vector2.ClampMagnitude(movimiento * velocidad, velocidad);
         }
+    }
+
+    /// <summary>
+    /// Controla el ataque del ziccboi y la espera tras este
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator ZiccboiAtaca()
+    {
+        anim.Play("EspadaZiccboiAtaca", -1, 0); //Se reproduce la animación de ataque de la espada
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(-1).length + tiempoEspera); //Se espera a que acabe + un tiempo de espera a elegir
+        atacando = false; //Se pone el ataque a false para poder volver a atacar
     }
 }
