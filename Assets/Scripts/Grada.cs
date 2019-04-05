0a395b74c8ceef122e86baf5cba68aa645a4c262
jsalva@ucm.es <GitHunTop10>
2019-04-05 12:49:24 +0200

    En proceso de decorar el exterior de la arena
    ntentando hacer una grada animada
    -js

---
+++ Assets/Scripts/Grada.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grada : MonoBehaviour {
+
+    Animator anim;
+    int empezar;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        empezar = Random.Range(0, 1);
+        StartCoroutine("empezarAnimacion");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerable empezarAnimacion()
+    {
+        yield return new WaitForSeconds(empezar);
+        anim.SetTrigger("Mover");
+    }
+}
