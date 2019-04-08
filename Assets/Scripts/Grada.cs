4b8b2000d62349fb2357922e6b4f2ba4636c35f3
Juanma <jsalva@ucm.es>
2019-04-08 18:51:33 +0200

    Nueva arena terminada
    ya hay nuevas paredes, gradas y decoracion añadida
    -js

--- Assets/Scripts/Grada.cs
+++ Assets/Scripts/Grada.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Grada : MonoBehaviour {
 
+    SpriteRenderer sprite;
     Animator anim;
     float empezar;
 
 	// Use this for initialization
 	void Start () {
+        sprite = GetComponent<SpriteRenderer>();
+        float posiciony = (transform.parent.position.y) + 30;
+        sprite.sortingOrder = Mathf.RoundToInt(posiciony*(-1));
         anim = GetComponent<Animator>();
         empezar = Random.Range(0f, 1.5f);
         StartCoroutine(Animacion());
