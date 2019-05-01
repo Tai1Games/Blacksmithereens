f33c6f805a3dcde2b4ff2bbbca905176e4dbe725
Juanma <jsalva@ucm.es>
2019-05-01 12:35:55 +0200

    Solo puedes hacer daño una vez por animacion a cada enemigo
    Aun no esta terminado, la implementacion la tengo que hacer mas eficiente y con los materiales del martillo no funciona

--- Assets/Scripts/Armas/HacerDaño.cs
+++ Assets/Scripts/Armas/HacerDaño.cs
@@ -6,26 +6,43 @@ public class HacerDaño : MonoBehaviour {
 
     public int daño;
 
+    private Collider2D[] enemigosDañados;
+    private int pos=0;
+
 	// Use this for initialization
 	void Start () {
+        enemigosDañados = new Collider2D[3];
     }
 
     // Update is called once per frame
     void Update () {
 		
 	}
-
+    
 
     private void OnTriggerEnter2D(Collider2D col)
-    {
+    {        
         VidaEnemigo vida;
         vida = col.GetComponent<VidaEnemigo>();   //coge referencia al objeto colisionado
+        bool dañado = false;
 
-        if (vida != null)
+        for(int i = 0; i < enemigosDañados.Length; i++)
+        {
+            if (enemigosDañados[i] == col) dañado = true;
+        }
+
+
+        if (vida != null && !dañado)
         {
             vida.RestaVida(daño);
+            enemigosDañados[pos] = col;
+            pos++;
         }
     }
 
-
+    public void AtaqueTerminado()
+    {
+        enemigosDañados = new Collider2D[3];
+        pos = 0;
+    }
 }
