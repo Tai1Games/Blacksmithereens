da87b7c23bde440488e0ba2b9b896114deb68eed
Juanma <jsalva@ucm.es>
2019-05-02 11:57:56 +0200

    Las armas solo pueden dañar una vez por enemigo por animacion
    Todas las armas ahora solo dañan al enemigo una vez por animacion, incluido el martillo con los materiales

--- Assets/Scripts/Armas/HacerDaño.cs
+++ Assets/Scripts/Armas/HacerDaño.cs
@@ -8,6 +8,7 @@ public class HacerDaño : MonoBehaviour {
 
     private Collider2D[] enemigosDañados;
     private int pos=0;
+    private bool dañado;
 
 	// Use this for initialization
 	void Start () {
@@ -24,25 +25,46 @@ public class HacerDaño : MonoBehaviour {
     {        
         VidaEnemigo vida;
         vida = col.GetComponent<VidaEnemigo>();   //coge referencia al objeto colisionado
-        bool dañado = false;
+        dañado = false;
 
-        for(int i = 0; i < enemigosDañados.Length; i++)
+        for (int i = 0; i < enemigosDañados.Length; i++)  //busca en el array de enemigos a ver si ese enemigo ya esta registrado o no
         {
             if (enemigosDañados[i] == col) dañado = true;
         }
 
-
         if (vida != null && !dañado)
         {
             vida.RestaVida(daño);
+            System.Array.Resize(ref enemigosDañados, pos+1);  //añade una posicion al array
             enemigosDañados[pos] = col;
             pos++;
         }
     }
 
+    /// <summary>
+    /// Busca si el enimigo que se le pasa ya se le ha tocado durante esa animacion
+    /// provisinalmente es bool por que es necesario para el martillo, tengo que preguntar a guille si esto es legal o no
+    /// </summary>
+    /// <param name="col"></param>
+    /// <returns></returns>
+    public bool BuscarEnemigo(Collider2D col)
+    {
+
+        for (int i = 0; i < enemigosDañados.Length; i++)
+        {
+            if (enemigosDañados[i] == col) dañado = true;
+        }
+
+        return dañado;
+    }
+
+
+    /// <summary>
+    /// Una vez se ha terminado la animacion llama a este metodo que resetea los parametros para el proximo ataque
+    /// </summary>
     public void AtaqueTerminado()
     {
-        enemigosDañados = new Collider2D[3];
+        enemigosDañados = new Collider2D[0];
         pos = 0;
     }
 }
--- Assets/Scripts/Armas/MartilloGanarMat.cs
+++ Assets/Scripts/Armas/MartilloGanarMat.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class MartilloGanarMat : MonoBehaviour {
 
+    private HacerDaño hd;
 
 	// Use this for initialization
 	void Start () {
-		
+        hd = GetComponent<HacerDaño>();
 	}
 	
 	// Update is called once per frame
@@ -17,9 +18,14 @@ public class MartilloGanarMat : MonoBehaviour {
 
     void OnTriggerEnter2D (Collider2D objeto)
     {
-        //Al colisionar con el enemigo, activa su método para obtener materiales adicionales
-        EnemigoDropeaMatMartillo sumaMat;
-        sumaMat = objeto.GetComponent<EnemigoDropeaMatMartillo>();
-        if (sumaMat != null) sumaMat.OtorgarMat();
+        bool tocado = hd.BuscarEnemigo(objeto);  //mira si durante esa animacion ya le ha dado a ese enemigo o no
+
+        if (tocado)
+        {
+            //Al colisionar con el enemigo, activa su método para obtener materiales adicionales
+            EnemigoDropeaMatMartillo sumaMat;
+            sumaMat = objeto.GetComponent<EnemigoDropeaMatMartillo>();
+            if (sumaMat != null) sumaMat.OtorgarMat();
+        }
     }
 }
