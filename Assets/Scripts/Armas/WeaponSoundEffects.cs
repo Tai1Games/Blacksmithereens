2ae682e4ecdf2f65ce20286182dc2ea6f0199d5a
Álvar <aldomi04@ucm.es>
2019-05-16 18:47:58 +0200

    Sound Effects

---
+++ Assets/Scripts/Armas/WeaponSoundEffects.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSoundEffects : MonoBehaviour {
+
+    public AudioClip soundEffect;
+
+    public AudioSource soundSource;
+
+	void Start () {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnEnable()
+    {
+        soundSource.Play();
+    }
+}
